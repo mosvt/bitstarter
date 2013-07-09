ea7ba36ef3bc32466049a2f649db4d383e4a3122
Maura Crawford <m5craw@yahoo.com>
2013-07-09 02:20:44 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var file = '/index.html';
+
+var fileContent = buf.toString(fs.readFileSync(file));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
